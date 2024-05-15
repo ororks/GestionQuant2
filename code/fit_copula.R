6204a79af6ef3c4f0e0f2ad679ec0f00c5ac45f2
roman schwalek <roman.herce@gmail.com>
2024-05-16 01:05:58 +0200

    estimation VaR possible mais valeurs buggés

--- code/fit_copula.R
+++ code/fit_copula.R
@@ -1,42 +1,40 @@
-if (!require("copula")) {
-  install.packages("copula", dependencies=TRUE)
-  library(copula)
+# Load necessary library
+Sys.setlocale("LC_ALL", "English_United States.UTF-8")
+#options(encoding = "UTF-8")
+library(copula)
+library(rmgarch)
+
+# Define the function fit_copula
+fit_copula <- function(residuals_data, copula_type = "Normal") {
+  # Define the copula model based on user input
+  copula_model <- switch(copula_type,
+                         "Normal" = normalCopula(dim = 2),
+                         "Student" = tCopula(dim = 2),
+                         "Plackett" = plackettCopula(param = 2),
+                         "Clayton" = claytonCopula(dim = 2),
+                         "rotClayton" = rotCopula(claytonCopula(dim = 2), 180),
+                         "Frank" = frankCopula(dim = 2),
+                         "Gumbel" = gumbelCopula(dim = 2),
+                         "rotGumbel" = rotCopula(gumbelCopula(dim = 2), 180),
+                         normalCopula(dim = 2)) # Default to Normal Copula
+  
+  # Load data from dataframe assuming the names match those in the dataframe
+  u <- pnorm(residuals_data[, "Residuals1"])
+  v <- pnorm(residuals_data[, "Residuals2"])
+  
+  u_sorted <- sort(u)
+  v_sorted <- sort(v)
+  
+  # Fit the copula to the data
+  fit <- fitCopula(copula_model, cbind(u, v), method = "ml")
+  
+  # Simulate new data
+  simulated <- rCopula(10000, fit@copula)
+  
+  # Transform simulated data back using quantile function of normal distribution
+  simulated_normal <- cbind(qnorm(simulated[,1]), qnorm(simulated[,2]))
+  
+  return(simulated_normal)
 }
 
-tryCatch({
-  residuals <- read.csv("residuals.csv")
-}, error = function(e) {
-  cat("Error reading the CSV file: ", e$message, "\n")
-  quit("no")
-})
-
-if (!("Residuals1" %in% names(residuals)) || !("Residuals2" %in% names(residuals))) {
-  cat("CSV file does not contain required columns.\n")
-  quit("no")
-}
-
-u <- pnorm(residuals$Residuals1)
-v <- pnorm(residuals$Residuals2)
-
-
-copula_model <- switch(copula_type,
-                       "Normal" = normalCopula(dim = 2),
-                       "Student" = tCopula(dim = 2),
-                       "Plackett" = plackettCopula(param = 2),
-                       "Clayton" = claytonCopula(dim = 2),
-                       "rotClayton" = rotCopula(claytonCopula(dim = 2)),
-                       "Frank" = frankCopula(dim = 2),
-                       "Gumbel" = gumbelCopula(dim = 2),
-                       "rotGumbel" = rotCopula(gumbelCopula(dim = 2)),
-                       normalCopula(dim = 2))  # Default case
-
-suppressMessages(suppressWarnings({
-  fit <- fitCopula(copula_model, data = cbind(u, v), method = "ml")
-  fit_summary <- capture.output(print(summary(fit)))
-  for (line in fit_summary) {
-    print(enc2utf8(line))
-  }
-}))
-
-fit_results <- summary(fit)
-assign('fit_results', fit_results, envir = .GlobalEnv)
+#copula_dens <- fit_copula(residuals, copula_type = "Normal")
--- code/garch_model.py
+++ code/garch_model.py
@@ -1,27 +1,46 @@
-import pandas as pd
-import numpy as np
+
 from scipy.optimize import minimize
-import matplotlib.pyplot as plt
-from scipy.stats import norm
 import rpy2.robjects as robjects
 from rpy2.robjects import pandas2ri
+from rpy2.rinterface_lib import callbacks
+from tqdm import tqdm
+import pandas as pd
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+matplotlib.use("Qt5Agg")
 pandas2ri.activate()
 import os
 
+
+def rpy2_output_error(output):
+    try:
+        decoded_output = output.decode('utf-8')
+        print(decoded_output)
+    except UnicodeDecodeError as e:
+        print("R output contained characters that could not be decoded:", e)
+
+# Override the default error handling and output
+callbacks.consolewrite_print = rpy2_output_error
+callbacks.consolewrite_warnerror = rpy2_output_error
+
 class Garch():
-    def __init__(self, returns, theta, copula_garch: bool = False, returns2=None, copula_type='Normal'):
+    def __init__(self, returns, price, theta, copula_garch: bool = False, returns2=None, price2=None, copula_type='Normal', h=None):
 
         self.returns = np.array(returns)
+        self.price = price
         self.theta = np.array(theta)
         self.copula_type = copula_type
-        self.setup_parameters(self.returns)
+        self.setup_parameters(self.returns, h)
+        self.h = h
         if copula_garch:
             self.returns2 = np.array(returns2)
-            self.setup_parameters(self.returns2, suffix='2')
+            self.price2 = price2
+            self.setup_parameters(self.returns2, self.h, suffix='2')
         self.copula_garch = copula_garch
-        self.results_copula = self.fit_copula_garch()
 
-    def setup_parameters(self, returns, suffix=''):
+
+    def setup_parameters(self, returns, h, suffix=''):
         setattr(self, f'var_incondi{suffix}', np.var(returns))
         theta_opti, _ = self.optim(returns, getattr(self, f'var_incondi{suffix}'))
         setattr(self, f'theta_opti{suffix}', theta_opti)
@@ -29,6 +48,29 @@ class Garch():
         setattr(self, f'var_condi{suffix}', var_condi)
         residuals = self.compute_residuals(theta_opti, returns, var_condi)
         setattr(self, f'residuals{suffix}', residuals)
+        predicted_variance = self.predict_variance(returns,
+                                                   theta_opti,
+                                                   var_condi,
+                                                   getattr(self, f'var_incondi{suffix}'),
+                                                   h)
+        setattr(self, f'predicted_variance{suffix}', predicted_variance)
+        predicted_mean = self.predict_mean(theta_opti, h)
+        setattr(self, f'predicted_mean{suffix}', predicted_mean)
+
+    def get_predicted_mean(self, suffix=''):
+        return getattr(self, f'predicted_mean{suffix}')
+
+    def get_var_condi(self, suffix=''):
+        return getattr(self, f'var_condi{suffix}')
+
+    def get_var_incondi(self, suffix=''):
+        return getattr(self, f'var_incondi{suffix}')
+
+    def get_theta_opti(self, suffix=''):
+        return getattr(self, f'theta_opti{suffix}')
+
+    def get_predicted_variance(self, suffix=''):
+        return getattr(self, f'predicted_variance{suffix}')
 
     def model(self, theta, returns, var_incondi):
         mu, alpha, beta = theta
@@ -62,6 +104,22 @@ class Garch():
         std_dev = np.sqrt(var_condi)
         return (returns - mu) / std_dev[1:]
 
+    def predict_mean(self, theta_opti, h):
+        mu, _, _ = theta_opti
+        predicted_mean = [mu] * h
+        return predicted_mean
+
+    def predict_variance(self, returns, theta_opti, var_condi, var_incondi, h):
+        mu, alpha, beta = theta_opti
+        w = (1 - alpha - beta) * var_incondi
+        var_last = w + alpha*((returns[-1]-mu)**2) + beta*var_condi[-1]
+        variance_predictions = [var_last]
+        for h in range(1, h):
+            variance_prediction = (w * np.sum([(alpha+beta)**i for i in range(h-1)]) +
+                                   (alpha+beta)**(h-1) * (alpha*((returns[-1]-mu)**2)+beta*var_condi[-1]))
+            variance_predictions.append(variance_prediction)
+        return variance_predictions
+
     def save_residuals(self, file_name='residuals.csv'):
         if self.copula_garch and hasattr(self, 'residuals2'):
 
@@ -71,7 +129,7 @@ class Garch():
                     'Residuals2': self.residuals2
                 })
 
-                directory = "C:/Users/roman/OneDrive/Bureau/M2 EIF/S2/Projet_GQ2/code"
+                directory = "C:/Users/roman/OneDrive/Bureau/M2EIF/S2/Projet_GQ2/code"
                 full_path = os.path.join(directory, file_name)
 
                 if not os.path.exists(directory):
@@ -87,23 +145,135 @@ class Garch():
         else:
             print("Résidus pas dispos pour les deux séries")
 
-    def fit_copula_garch(self):
+    def fit_copula_garch(self, residuals_1, residuals_2):
         self.save_residuals()
-        if os.path.exists('residuals.csv'):
-            try:
-                robjects.r.assign("copula_type", self.copula_type)
+        script_path = "C:/Users/roman/OneDrive/Bureau/M2EIF/S2/Projet_GQ2/code/fit_copula.R"
 
-                robjects.r.source('fit_copula.R')
+        # Load R script
+        robjects.r(f'source("{script_path}")')
 
-                if 'fit_results' in robjects.globalenv:
-                    fit_results = robjects.globalenv['fit_results']
-                    print(fit_results if isinstance(fit_results, str) else fit_results.r_repr())
-                else:
-                    print("Résultats pas trouvés dans l'env R")
-            except Exception as e:
-                print(f"{str(e)}")
-        else:
-            print("Fichier des résidus pas trouvé")
+        # Prepare data frame in Python and convert to R
+        df = pd.DataFrame({
+            'Residuals1': residuals_1,
+            'Residuals2': residuals_2
+        })
+        r_dataframe = pandas2ri.py2rpy(df)
+
+        # Call R function directly with converted data frame
+        try:
+            r_fit_copula = robjects.globalenv['fit_copula']
+            result = r_fit_copula(r_dataframe, self.copula_type)
+            simulated_residuals = np.array(result)  # Ensure result is a numpy array
+            return simulated_residuals
+        except Exception as e:
+            print("Error during copula fitting:", e)
+            return None
+
+    def main_loop(self):
+        T = len(self.returns)
+        window_size = 1135
+        perc95_VaR = []
+        perc99_VaR = []
+        print("Start estimation of the VaR")
+        for i in tqdm(range(500)):
+            print(f'iteration {i}/500')
+            start_index = i
+            end_index = window_size + i
+            returns_window = self.returns[start_index:end_index]
+            returns2_window = self.returns2[start_index:end_index]
+
+            theta_opti_1, _ = self.optim(returns_window, self.get_var_incondi())
+            var_condi_1 = self.fit(theta_opti_1, returns_window, self.get_var_incondi())
+            print(f'var_condi 1 : {var_condi_1}')
+            residuals_1 = self.compute_residuals(theta_opti_1, returns_window, var_condi_1)
+            var_condi_1_pred = self.predict_variance(returns_window, theta_opti_1, var_condi_1, self.get_var_incondi(), h=1)
+            print(f'var_condi_pred 1 : {var_condi_1_pred}')
+            mean_1_pred = self.predict_mean(theta_opti_1, 1)
+            print(f'moyenne predite 1 : {mean_1_pred}')
+            theta_opti_2, _ = self.optim(returns2_window, self.get_var_incondi(suffix='2'))
+            var_condi_2 = self.fit(theta_opti_2, returns2_window, self.get_var_incondi(suffix='2'))
+            print(f'var_condi 2 : {var_condi_2}')
+            residuals_2 = self.compute_residuals(theta_opti_2, returns2_window, var_condi_2)
+            var_condi_2_pred = self.predict_variance(returns2_window, theta_opti_2, var_condi_2,
+                                                     self.get_var_incondi(suffix='2'), h=1)
+            print(f'var_condi_pred 2 : {var_condi_2_pred}')
+            mean_2_pred = self.predict_mean(theta_opti_2, 1)
+            print(f'moyenne predite 2 : {mean_2_pred}')
+            MC_residuals = self.fit_copula_garch(residuals_1, residuals_2)
+
+            portfolio = []
+            for j in range(len(MC_residuals)):
+                simu_resid_1 = MC_residuals[j, 0]
+                print(f'resid 1 simu : {simu_resid_1}')
+                simu_resid_2 = MC_residuals[j, 1]
+                print(f'resid 2 simu : {simu_resid_2}')
+                simu_return_1 = np.sqrt(var_condi_1_pred) * simu_resid_1 + mean_1_pred
+                print(f'rend simu 1 : {simu_return_1}')
+                simu_return_2 = np.sqrt(var_condi_2_pred) * simu_resid_2 + mean_2_pred
+                print(f'rend simu 2 : {simu_return_2}')
+                price_1_t_1 = self.price[end_index - 1]
+                print(f'prix en t-1 : {price_1_t_1}')
+                price_2_t_1 = self.price2[end_index - 1]
+                print(f'prix 2 en t-1 : {price_2_t_1}')
+                portfolio_value = ((np.exp(simu_return_1)*price_1_t_1 - price_1_t_1) + (
+                            np.exp(simu_return_2)*price_2_t_1 - price_2_t_1))/2
+                print(f'partie 1 du portefeuille : {(0.5 * price_1_t_1 * (np.exp(simu_return_1*100) - 1))}')
+                print(f'partie 2 du portefeuille : {(0.5 * price_2_t_1 * (np.exp(simu_return_2*100) - 1))}')
+                print(f'valeur profit-loss :{portfolio_value}')
+                portfolio.append(portfolio_value)
+
+            portfolio.sort()
+            perc95_VaR.append(np.percentile(portfolio, 5))
+            perc99_VaR.append(np.percentile(portfolio, 1))
+
+        return perc95_VaR, perc99_VaR
+
+    # def get_copula_returns(self):
+    #     predicted_var = self.predict_variance()
+    #     predicted_var2 = self.get_predicted_variance(suffix='2')
+    #     predicted_mean = self.get_predicted_mean()
+    #     predicted_mean2 = self.get_predicted_mean(suffix='2')
+    #     residuals_copula = np.hsplit(self.results_copula, 2)[0]
+    #     residuals_copula2 = np.hsplit(self.results_copula, 2)[1]
+    #     copula_returns = np.sum(np.dot(np.sqrt(predicted_var), residuals_copula.flatten()), predicted_mean)
+    #     copula_returns2 = np.sum(np.dot(np.sqrt(predicted_var2), residuals_copula2), predicted_mean2)
+    #     df = np.concatenate([copula_returns, copula_returns2], axis=1)
+    #     return df
+
+
+    # def calculate_cdf(self, residuals):
+    #     sorted_residuals = np.sort(residuals)
+    #     cdf = norm.cdf(sorted_residuals)
+    #     return cdf
+    #
+    # def copula_density(self, u1, u2):
+    #     # This function should return the copula density based on the copula_density_matrix
+    #     # Placeholder example for a Gaussian copula
+    #     from scipy.stats import norm
+    #     rho = np.corrcoef(u1, u2)[0, 1]  # correlation coefficient
+    #     return np.exp(-0.5 * ((u1 ** 2 - 2 * rho * u1 * u2 + u2 ** 2) / (1 - rho ** 2))) / (
+    #                 2 * np.pi * np.sqrt(1 - rho ** 2))
+    #
+    # def integrand(self, u1, u2, VaR_t, pi, cdf1, cdf2, pdf1, pdf2):
+    #     z_t = (VaR_t / pi) - ((1 - pi) / pi) * u2
+    #     return self.copula_density(cdf1(u1), cdf2(u2)) * pdf1(u1) * pdf2(u2)
+    #
+    # def equation_to_solve(self, VaR_t, alpha, pi, cdf1, cdf2, pdf1, pdf2):
+    #     integral_result, _ = dblquad(lambda u2, u1: self.integrand(u1, u2, VaR_t, pi, cdf1, cdf2, pdf1, pdf2),
+    #                                  -np.inf, np.inf,
+    #                                  lambda u1: -np.inf, lambda u1: np.inf)
+    #     return integral_result - alpha
+    #
+    # def calculate_var(self, alpha, pi=0.5):
+    #     cdf1, pdf1 = self.calculate_cdf_and_pdf(self.residuals)
+    #     cdf2, pdf2 = self.calculate_cdf_and_pdf(self.residuals2)
+    #
+    #     VaR_t_initial_guess = 1.0
+    #
+    #     VaR_t_solution = fsolve(self.equation_to_solve, VaR_t_initial_guess, args=(alpha, pi, cdf1, cdf2, pdf1, pdf2))[
+    #         0]
+    #     return VaR_t_solution
+
 
     def plot(self):
         fig, ax = plt.subplots(figsize=(12, 6))
@@ -122,27 +292,33 @@ class Garch():
         plt.show()
 
 if __name__ == "__main__":
-    datas = pd.read_csv(r'C:\Users\roman\OneDrive\Bureau\M2 EIF\S2\Projet_GQ2\datas\SP500NASDAQ.csv')
-
-    df = pd.DataFrame(datas)
-    df['DATE'] = pd.to_datetime(df['DATE'])
-    df.set_index('DATE', inplace=True)
-    df["SP500"] = pd.to_numeric(df["SP500"], errors="coerce")
-    df = df[df["SP500"] != "."]
-    df = df.dropna()
-    SP500_returns = np.log(df['SP500']).diff().dropna() * 100
-
-    mu = np.mean(SP500_returns)
+    df = pd.read_excel('C:/Users/roman/OneDrive/Bureau/M2EIF/S2/Projet_GQ2/datas/SP500NASDAQ2.xls', header=0,
+                       index_col=0)
+    NASDAQ_price = df.iloc[:, 0]
+    SP500_price = df.iloc[:, 1]
+    NASDAQ_logreturn = np.log(df['NASDAQCOM']).diff().dropna()
+    SP500_logreturn = np.log(df['SP500']).diff().dropna()
+    mu = np.mean(SP500_logreturn)
     initial_params = [mu, 0.1, 0.1]
-    df["NASDAQCOM"] = pd.to_numeric(df["NASDAQCOM"], errors="coerce")
-    df = df[df["NASDAQCOM"] != "."]
-    df = df.dropna()
-    data_test = df["NASDAQCOM"].to_numpy(dtype=np.float64)
-    NASDAQ_returns = np.log(df['NASDAQCOM']).diff().dropna() * 100
-    print(NASDAQ_returns)
-    model = Garch(returns=SP500_returns,
+    model = Garch(returns=SP500_logreturn,
+                  price=SP500_price,
                   theta=initial_params,
                   copula_garch=True,
-                  returns2=NASDAQ_returns,
-                  copula_type="rotGumbel")
-    print(model.results_copula)
+                  returns2=NASDAQ_logreturn,
+                  price2=NASDAQ_price,
+                  copula_type="Plackett",
+                  h=1)
+
+    perc95_VaR, perc99_VaR = model.main_loop()
+    print(f"95% VaR: {perc95_VaR}")
+    print(f"99% VaR: {perc99_VaR}")
+
+    plt.figure(figsize=(10, 6))
+    plt.plot(perc95_VaR, label='95% VaR', color='blue')
+    plt.plot(perc99_VaR, label='99% VaR', color='red')
+    plt.xlabel('Time Period')
+    plt.ylabel('VaR Value')
+    plt.title('Value at Risk (VaR) over Time')
+    plt.legend()
+    plt.grid(True)
+    plt.show()
